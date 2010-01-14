fbb6b454e6c1fb8f9f60f4052f9a13ca9c39d12c
Corey Oordt <coordt@washingtontimes.com>
2010-01-14 12:10:41 -0500

    Added a setup function to setup.py. Why didn't I do this earlier?

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='epub-creator',
+      version='0.1',
+      description='Generate ePub documents using Django',
+      long_description='',
+      author='Corey Oordt',
+      author_email='coordt@washingtontimes.com',
+      url='http://opensource.washingtontimes.com/projects/epub-creator/',
+      packages=['epub'],
+      classifiers=['Development Status :: 3 - Alpha',
+          ],
+      )
